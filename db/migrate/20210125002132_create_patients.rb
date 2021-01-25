9ada9a5422ee41e5f064deace7bf6620bb5b2b42
Michael Fuget <michaelfuget@gmail.com>
2021-01-25 00:43:59 +0000

    Automatically backed up by Learn

--- db/migrate/20210125002132_create_patients.rb
+++ db/migrate/20210125002132_create_patients.rb
@@ -2,7 +2,7 @@ class CreatePatients < ActiveRecord::Migration[5.0]
   def change
     create_table :patients do |t|
       t.string :name
-      t.integer :name
+      t.integer :age
 
       t.timestamps
     end
