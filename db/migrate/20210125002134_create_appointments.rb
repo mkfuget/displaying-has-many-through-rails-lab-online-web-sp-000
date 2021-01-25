42822af366949502463085e0be4d7de3cb952ed3
Michael Fuget <michaelfuget@gmail.com>
2021-01-25 00:40:07 +0000

    Automatically backed up by Learn

--- db/migrate/20210125002134_create_appointments.rb
+++ db/migrate/20210125002134_create_appointments.rb
@@ -3,7 +3,7 @@ class CreateAppointments < ActiveRecord::Migration[5.0]
     create_table :appointments do |t|
       t.integer :doctor_id
       t.integer :patient_id
-      t.DateTime :appointment_datetime
+      t.datetime :appointment_datetime
 
       t.timestamps
     end
